e20380ea5c802b5e88a438f2f9d62cb65731326f
Tamal Das <gyansujan69@gmail.com>
2021-11-10 10:39:44 +0530

    Database updated.

--- server/index.js
+++ server/index.js
@@ -14,8 +14,8 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
-const PORT = process.env.PORT|| 5000;
+const CONNECTION_URL = 'mongodb+srv://tamal:tamaldas69@cluster0.nzhcp.mongodb.net/classbook?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
