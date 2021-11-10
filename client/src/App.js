35a55c461fa284c2f96683bbe3aaedbf3e1a2d38
Tamal Das <gyansujan69@gmail.com>
2021-11-10 11:02:55 +0530

    Updated frontend.

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import { getPosts } from './actions/posts';
 import useStyles from './styles';
-import memories from './images/memories.png';
+import book from './images/book.png'
 
 const App = () => {
   const [currentId, setCurrentId] = useState(0);
@@ -20,8 +20,8 @@ const App = () => {
   return (
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-        <img className={classes.image} src={memories} alt="icon" height="60" />
+        <Typography className={classes.heading} variant="h2" align="center">Class-Book</Typography>
+        <img className={classes.image} src={book} alt="icon" height="60" />
       </AppBar>
       <Grow in>
         <Container>
