3cd20346ae6fcf63891d20c59db27c092477f626
Tamal Das <gyansujan69@gmail.com>
2021-11-19 11:44:10 +0530

    Built for deployments.

--- server/index.js
+++ server/index.js
@@ -1,24 +1,40 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
 
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-
-import postRoutes from './routes/posts.js';
+import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes);
+app.use("/posts", postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://tamal:tamaldas69@cluster0.nzhcp.mongodb.net/classbook?retryWrites=true&w=majority';
+const CONNECTION_URL =
+  "mongodb+srv://tamal:tamaldas69@cluster0.nzhcp.mongodb.net/classbook?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+// deployment in heroku
+if (process.eventNames.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
   .catch((error) => console.log(`${error} did not connect`));
 
-mongoose.set('useFindAndModify', false);
+mongoose.set("useFindAndModify", false);
