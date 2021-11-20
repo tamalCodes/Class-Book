563fe1dfaf6cfa92768f69d01c329aebcac27792
Tamal Das <gyansujan69@gmail.com>
2021-11-20 11:00:06 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+
+// const process.env.CONNECTION_URL =
+//   "mongodb+srv://tamal:tamaldas69@cluster0.nzhcp.mongodb.net/classbook?retryWrites=true&w=majority";
+
+app.get("/", (req, res) => {
+  res.send("Hello to the Class-book API");
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set("useFindAndModify", false);
